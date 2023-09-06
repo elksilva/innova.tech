cec3362ee9732ea1fb980f973bf8a86d45af83a2
elksilva <elksilva@gmail.com>
2023-09-06 13:05:55 -0500

    Deploy Railway Not Spinner

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import BarraInfo from '@/componets/BarraInfo'
 import Logonav from '@/componets/Logonav'
 import Btnwhastapp from '@/componets/Btnwhastapp'
 import Footer from '@/componets/Footer'
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import './globals.css'
 import Spinner from "@/componets/Spinner"
 
@@ -20,23 +20,23 @@ export default function RootLayout({ children, pageProps }) {
 
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Simula la carga de contenido (esto puede ser una llamada a una API u otra carga de datos)
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Simulamos una carga de 2 segundos
-  }, []);
+  // useEffect(() => {
+  //   // Simula la carga de contenido (esto puede ser una llamada a una API u otra carga de datos)
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000); // Simulamos una carga de 2 segundos
+  // }, []);
 
   return (
     <html lang="es">
       <body className={fuentePrimaria.className}>
-        <div>
+        {/* <div>
           {loading ? (
             <Spinner/>
           ) : (
             <div {...pageProps} />
           )}
-        </div>
+        </div> */}
         <NextTopLoader  
           color='#ff0404'
           showSpinner={false}
